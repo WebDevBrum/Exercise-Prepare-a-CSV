43347f3f36c60f7afbf14bfd53d808d863a32c5b
Scott Davies <scottdavies1983@gmail.com>
2021-06-29 13:47:06 +0100

    my solution , converts object to new object and writes to new file

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,41 @@
 import express from 'express';
+import { promises as fsPromises } from 'fs';
+import csv from 'csvtojson';
 
 const app = express();
 const port = 3000;
 
+const inputFile = './users.csv';
+const outputFile = 'users.json';
 
 // start the Express server
 app.listen(port, () => {
   console.log(`server started at http://localhost:${port}`);
+});
+
+//define a route handler for the dafault home page
+
+app.get('/convert', (req, res) => {
+  res.send('conversion in progress!')
+  csv()
+    .fromFile(inputFile)
+    .then((data) => {
+      console.log(data);
+      let newData = data.map((item: {
+        first_name: string; last_name: string; phone: string;
+
+      }) => {
+        let first = item.first_name;
+        let last = item.last_name;
+        let phone = item.phone;
+        if (item.phone === "") {
+          phone = 'missing data';
+        }
+        // return a new object
+        return { first, last, phone };
+
+      });
+      console.log('converted: ' + JSON.stringify(newData))
+      fsPromises.writeFile(outputFile, JSON.stringify(newData));
+    })
 });
